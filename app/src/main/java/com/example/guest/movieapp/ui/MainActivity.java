a0115e51a2e2584552e2e413f07430a4c2774768
trevorelvey <trevor.elvey@gmail.com>
2016-04-27 11:52:42 -0700

    layout search result activity

--- app/src/main/java/com/example/guest/movieapp/ui/MainActivity.java
+++ app/src/main/java/com/example/guest/movieapp/ui/MainActivity.java
@@ -5,22 +5,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.guest.movieapp.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
-    private Button mButtonSearch;
+    @Bind(R.id.buttonSearch) Button mButtonSearch;
+    @Bind(R.id.searchField) EditText mSearchField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        mButtonSearch = (Button) findViewById(R.id.buttonSearch);
         mButtonSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String location = mSearchField.getText().toString();
                 Intent intent = new Intent(MainActivity.this, SearchResultActivity.class);
+                intent.putExtra("title", title);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/guest/movieapp/ui/SearchResultActivity.java
+++ app/src/main/java/com/example/guest/movieapp/ui/SearchResultActivity.java
@@ -1,15 +1,29 @@
 package com.example.guest.movieapp.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.guest.movieapp.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class SearchResultActivity extends AppCompatActivity {
+    public static final String TAG = SearchResultActivity.class.getSimpleName();
+    @Bind(R.id.resultsText) TextView mResultsText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_result);
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        String title = intent.getStringExtra("title");
+        mResultsText.setText("Here are teh results for " + title);
+
+
     }
 }
