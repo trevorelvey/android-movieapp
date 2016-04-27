5454eb3c872bd8269a5744518e056fa958fa6819
trevorelvey <trevor.elvey@gmail.com>
2016-04-27 09:36:22 -0700

    add api tools

---
+++ app/src/main/java/com/example/guest/movieapp/Constants.java
@@ -0,0 +1,5 @@
+package com.example.guest.movieapp;
+
+public class Constants {
+    public static final String API_KEY = BuildConfig.API_KEY;
+}
--- app/src/main/java/com/example/guest/movieapp/ui/MainActivity.java
+++ app/src/main/java/com/example/guest/movieapp/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.guest.movieapp;
+package com.example.guest.movieapp.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.guest.movieapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
